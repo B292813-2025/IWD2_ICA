1c2e163208f86adb3b77ef27a75ead67f95e294f
Anonymous <anon@example.com>
2026-03-30 02:36:42 +0100

    Important info about this file added - please do not run

--- cleanup.php
+++ cleanup.php
@@ -1,4 +1,5 @@
 <?php
+// NOTE: Only run this to clear jobs - do not use as job 44 (I think) is what is used for example.php
 $data_dir = '/home/s2837201/public_html/ICA/data/';
 $dirs = glob($data_dir . 'job_*', GLOB_ONLYDIR);
 foreach ($dirs as $dir) {
